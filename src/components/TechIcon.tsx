d7e8bd6f487822a20d2eb84cfd59610113125ba5
hassaammgl <h455a4mmehtab@gmail.com>
2024-12-02 07:03:49 +0500

    adding profile img

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const TechIcon = ({ component }: { component: React.ElementType }) => {
-  const Componenet = component;
-  return (
-    <>
-      <Componenet className="size-10 fill-[url(#tech-icon-gradient)]" />
-      <svg className="size-0  absolute">
-        <linearGradient id="tech-icon-gradient">
-          <stop offset="0%" stop-color="rgb(110 231 183)" />
-          <stop offset="100%" stop-color="rgb(56 189 248)" />
-        </linearGradient>
-      </svg>
-    </>
-  );
+	const Componenet = component;
+	return (
+		<>
+			<Componenet className="size-10 fill-[url(#tech-icon-gradient)]" />
+			<svg className="size-0  absolute">
+				<linearGradient id="tech-icon-gradient">
+					<stop offset="0%" stopColor="rgb(110 231 183)" />
+					<stop offset="100%" stopColor="rgb(56 189 248)" />
+				</linearGradient>
+			</svg>
+		</>
+	);
 };
 
 export default TechIcon;
